4fcfe94bec0e7225a3fa6a6c4c175ffde835bfda
abderrahmane140 <bsarabderrahmane01@gmail.com>
2025-12-27 23:55:00 +0100

    create mothod for get all coaches and updating the edit method for checking the image

--- model/CoachProfile.php
+++ model/CoachProfile.php
@@ -3,15 +3,15 @@
 require_once __DIR__ . "/../core/Database.php";
 
 class CoachProfile {
-    private $id;
-    private $user_id;
-    private $description;
-    private $experience_years;
-    private $certifications;
-    private $photo;
+    private int $id;
+    private int $user_id;
+    private string $description;
+    private int $experience_years;
+    private string $certifications;
+    private ?string $photo;
 
 
-    public function __construct($description, $user_id, $experience_years, $certifications, $photo)
+    public function __construct(string $description, int $user_id, int $experience_years, string $certifications, string $photo)
     {
         $this->description = $description;
         $this->user_id = $user_id;
@@ -19,6 +19,11 @@ class CoachProfile {
         $this->certifications = $certifications;
         $this->photo = $photo;
     }
+
+    public function setId(int $id) : void {
+        $this->id = $id;
+    }
+
 
 
     public function save() : bool {
@@ -33,36 +38,86 @@ class CoachProfile {
         ]);
     }
 
-    public function update() : bool {
-        $pdo = Database::getConnection();
-        $stmt = $pdo->prepare("UPDATE coach_profiles SET         
-            description = :description,
-            experience_years = :experience_years,
-            certifications = :certifications,
-            photo = :photo
-            WHERE user_id = :user_id");
-        return $stmt->execute([
-                'user_id'=> $this->user_id,
-                'description'=> $this->description,
-                'experience_years' => $this->experience_years,
-                'certifications' => $this->certifications,
-                'photo' => $this->photo
-        ]);
-    }
+    public function update(): bool {
+            $pdo = Database::getConnection();
+
+            if ($this->photo === null) {
+                $stmt = $pdo->prepare("
+                    UPDATE coach_profiles SET
+                        description = ?,
+                        experience_years = ?,
+                        certifications = ?
+                    WHERE user_id = ?
+                ");
+
+                return $stmt->execute([
+                    $this->description,
+                    $this->experience_years,
+                    $this->certifications,
+                    $this->user_id
+                ]);
+            }
+
+            $stmt = $pdo->prepare("
+                UPDATE coach_profiles SET
+                    description = ?,
+                    experience_years = ?,
+                    certifications = ?,
+                    photo = ?
+                WHERE user_id = ?
+            ");
+
+            return $stmt->execute([
+                $this->description,
+                $this->experience_years,
+                $this->certifications,
+                $this->photo,
+                $this->user_id
+            ]);
+        }
 
 
-    public function getCoachById(int $user_id) {
+
+
+    public static function getCoachById(int $user_id) {
         $pdo = Database::getConnection();
         $stmt = $pdo->prepare("SELECT * FROM coach_profiles WHERE user_id = ?");
         $stmt->execute([$user_id]);
         return $stmt->fetch();
     }
 
-    public function checkCoach($user_id) {
+    public function exists($user_id) {
         $pdo = Database::getConnection();
         $stmt = $pdo->prepare("SELECT * FROM coach_profiles WHERE user_id = :user_id LIMIT 1");
         $stmt->execute(['user_id'=> $user_id]);
         return $profile = $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+
+
+    //get all the coachs for the user
+
+    public static function getAllCoaches(){
+        $pdo = Database::getConnection();
+        $stmt = $pdo->prepare("
+            SELECT 
+                c.id AS coach_profile_id,
+                c.user_id,
+                u.username,
+                c.description,
+                c.experience_years,
+                c.certifications,
+                c.photo,
+                GROUP_CONCAT(s.sport_name SEPARATOR ', ') AS sports
+            FROM coach_profiles c
+            INNER JOIN users u ON c.user_id = u.id
+            LEFT JOIN coach_sports cs ON cs.coach_profile_id = c.id
+            LEFT JOIN sports s ON s.id = cs.sport_id
+            WHERE u.role = 'coach'
+            GROUP BY c.id
+        ");
+
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
 
